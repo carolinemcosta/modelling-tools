191ca0e64a6845c62330c8ca29edf22684b1ee37
carolinemcosta <caluline@gmail.com>
2021-07-27 13:37:24 +0100

    "better formatting"

--- generate_mesh_from_seg.py
+++ generate_mesh_from_seg.py
@@ -1,121 +1,117 @@
-import numpy as np
-import os.path, time
-from shutil import copyfile
+import os
 
-def create_CGAL_config_file(script_name, image_name, mesh_name, myo_res, bath_res, bath_tag, 
-                            rv_pool_tag=-1 , lv_pool_tag=-1, facet_angle=30, facet_distance=0.5, 
-                            edge_size=0.5, cell_radius_edge_ratio=1.5, n_threads=8):
-  
-  ''' Creates CGAL configuration file for cgalmeshmultilabel, which is part of the cgalmesh packaged from the MUG
-      
-      Parameters: 
-        script_name (string): path+name of configuration file (extension: .yaml)
-        image_name (string): path+name of input image
-        mesh_name (string): path+name of output mesh
-        myo_res (float): target resolution of myocardium
-        bath_res (float): target resolution of surrounding bath
-        bath_tag (int): image tag identifying bath
-        
-      Optional parameters:
-        rv_pool_tag (int): image tag identifying RV blood pool (if present, otherwise same as bath_tag)
-        lv_pool_tag (int): image tag identifying LV blood pool (if present, otherwise same as bath_tag)
-        facet_angle (int): CGAL's facet_angle. Default = 30
-        facet_distance (float): CGAL's facet_distance. Default = 0.5
-        edge_size (float): CGAL's edge_size. Default = 0.5
-        cell_radius_edge_ratio (float): CGAL's cell_radius_edge_ratio. Default = 1.5
-        n_threads (int): number of threads to use while generating mesh. Default = 8
-        
-      Returns:
-        Writes out a yaml file with configuration parameters for cgalmeshmultilabel
-  '''   
-  
-  # set RV and LV blood pool tags depending on arguments passed: if not passed, set as bath_tag
-  if rv_pool_tag < 0: rv_pool_tag = bath_tag
-  if lv_pool_tag < 0: lv_pool_tag = bath_tag
-  
-  # build dictionary with config parameters
-  config = {"input_img": image_name,
-            "output_carp": %mesh_name,
-            "output_vtk": "%s.vtk"%mesh_name,
-            "facet_angle": facet_angle,
-            "facet_size": myo_res,
-            "facet_distance": facet_distance,
-            "cell_radius_edge_ratio": cell_radius_edge_ratio,
-            "cell_size": myo_res,
-            "edge_size": edge_size,
-            "bath_size": bath_res,
-            "bath_tag": bath_tag,
-            "rvp_tag": rv_pool_tag,
-            "lvp_tag": lv_pool_tag,
-            "tbb_threads": n_threads
-            }
-  
-  # write dictionary to text file
-  with open(script_name, 'w') as f:
-    print(config, file=f)  
+def write_cgal_config_file(script_name, image_name, mesh_name, myo_res, bath_res, bath_tag,
+                           rv_pool_tag=-1, lv_pool_tag=-1, facet_angle=30, facet_distance=0.5,
+                           edge_size=0.5, cell_radius_edge_ratio=1.5, n_threads=8):
+    """ Creates CGAL configuration file for cgalmeshmultilabel, which is part of the cgalmesh packaged from the MUG
+
+        Parameters:
+          script_name (string): path+name of configuration file (extension: .yaml)
+          image_name (string): path+name of input image
+          mesh_name (string): path+name of output mesh
+          myo_res (float): target resolution of myocardium
+          bath_res (float): target resolution of surrounding bath
+          bath_tag (int): image tag identifying bath
+          rv_pool_tag (int): image tag identifying RV blood pool (if present, otherwise same as bath_tag)
+          lv_pool_tag (int): image tag identifying LV blood pool (if present, otherwise same as bath_tag)
+          facet_angle (int): CGAL's facet_angle. Default = 30
+          facet_distance (float): CGAL's facet_distance. Default = 0.5
+          edge_size (float): CGAL's edge_size. Default = 0.5
+          cell_radius_edge_ratio (float): CGAL's cell_radius_edge_ratio. Default = 1.5
+          n_threads (int): number of threads to use while generating mesh. Default = 8
+
+        Returns:
+          Writes out a yaml file with configuration parameters for cgalmeshmultilabel
+    """
+
+    # set RV and LV blood pool tags depending on arguments passed: if not passed, set as bath_tag
+    if rv_pool_tag < 0:
+        rv_pool_tag = bath_tag
+    if lv_pool_tag < 0:
+        lv_pool_tag = bath_tag
+
+    # build dictionary with config parameters
+    config = {"input_img": image_name,
+              "output_carp": mesh_name,
+              "output_vtk": "{}.vtk".format(mesh_name),
+              "facet_angle": facet_angle,
+              "facet_size": myo_res,
+              "facet_distance": facet_distance,
+              "cell_radius_edge_ratio": cell_radius_edge_ratio,
+              "cell_size": myo_res,
+              "edge_size": edge_size,
+              "bath_size": bath_res,
+              "bath_tag": bath_tag,
+              "rvp_tag": rv_pool_tag,
+              "lvp_tag": lv_pool_tag,
+              "tbb_threads": n_threads
+              }
+
+    # write dictionary to text file
+    with open(script_name, 'w') as f:
+        print(config, file=f)
 
 
 def convert_nrrd_to_inr(segconvert_bin, image_name):
-  
-  ''' Converts NRRD image to INR format required by CGAL
-    Parameters:
-      segconvert_bin (string): path+name of segconvert binary
-      image_name (string): path+name of input image in NRRD format. File ends in .nrrd
-    Returns:
-      Writes out new image file in INR format with .inr extension
-  '''
-  
-  inr_image = image_name.split(".nrrd")[0] + ".inr"
-  
-  if os.path.isfile(image_name) and not os.path.isfile(inr_image):
-    print("Converting NRRD to INR format...")
-    cmd = "%s %s %s" % (segconvert_bin, image_name, inr_image)
-    os.system(cmd)  
-    
+    """ Converts NRRD image to INR format required by CGAL
+      Parameters:
+        segconvert_bin (string): path+name of segconvert binary
+        image_name (string): path+name of input image in NRRD format. File ends in .nrrd
+      Returns:
+        Writes out new image file in INR format with .inr extension
+    """
 
-def generate_cgal_multilabel_mesh(cgal_bin, mesh_name, config_script):
-  
-  ''' Generates CGAL mesh from a multilabel segmentation. 
-      Uses cgalmeshmultilabel (cgalmesh package) and a configuration file (.yaml)
-    Parameters:
-      cgal_bin (string): path+name of segconvert binary
-      mesh_name (string): path+basename of output mesh
-      config_script: name of .yaml configuration script for CGAL (create_CGAL_config_file)
-    Returns:
-      Writes out a CGAL mesh in both carp_txt and VTK formats
-  '''
-  
-  if not os.path.isfile(mesh_name + ".elem"):
-    print("Generating CGAL mesh...")
-    cmd = "%s %s"%(mesh_name, config_script)
-    os.system(cmd)  
+    inr_image = image_name.split(".nrrd")[0] + ".inr"
 
-  
+    if os.path.isfile(image_name) and not os.path.isfile(inr_image):
+        print("Converting NRRD to INR format...")
+        cmd = "{} {} {}".format(segconvert_bin, image_name, inr_image)
+        os.system(cmd)
+
+
+def generate_cgal_multi_label_mesh(cgal_bin, mesh_name, config_script):
+    """ Generates CGAL mesh from a multi label segmentation.
+        Uses cgalmeshmultilabel (cgalmesh package) and a configuration file (.yaml)
+      Parameters:s
+        cgal_bin (string): path+name of segconvert binary
+        mesh_name (string): path+basename of output mesh
+        config_script: name of .yaml configuration script for CGAL (create_CGAL_config_file)
+      Returns:
+        Writes out a CGAL mesh in both carp_txt and VTK formats
+    """
+
+    if not os.path.isfile(mesh_name + ".elem"):
+        print("Generating CGAL mesh...")
+        cmd = "{} {} {}".format(cgal_bin, mesh_name, config_script)
+        os.system(cmd)
+
+
 def smooth_myo_surface(meshtool_bin, mesh_name, smooth_mesh, myo_tags):
+    """ Smooths the myocardium surface of a mesh using meshtool
+      Parameters:
+        meshtool_bin (string): path+name of meshtool binary
+        mesh_name (string): path+basename of input mesh (expects carp_txt format)
+        smooth_mesh (string): path+basename of output smooth mesh (writes out in carp_txt format)
+        myo_tags (list): list of myocardium (including scar) mesh tags. Ex [1, 2, 3]
+      Returns:
+        Writes out new mesh files for smooth mesh in carp_txt format
+    """
 
-  ''' Smooths the myocardium surface of a mesh using meshtool
-    Parameters:
-      meshtool_bin (string): path+name of meshtool binary
-      mesh_name (string): path+basename of input mesh (expects carp_txt format)
-      smooth_mesh (string): path+basename of output smooth mesh (writes out in carp_txt format)
-      myo_tags (list): list of myocardium (including scar) mesh tags. Ex [1, 2, 3]
-    Returns:
-      Writes out new mesh files for smooth mesh in carp_txt format
-  '''
-  
-  if os.path.isfile(mesh_name + ".elem") and not os.path.isfile(smooth_mesh + ".elem"):
-    print("Smoothing myocardium surface...")
+    if os.path.isfile(mesh_name + ".elem") and not os.path.isfile(smooth_mesh + ".elem"):
+        print("Smoothing myocardium surface...")
 
-    # extract surface surrounding the myocardium
-    cmd = "%s extract surface -msh=%s -surf=%s-outer -op=%s -ofmt=carp_txt"%
-          (meshtool_bin, mesh_name, mesh_name, 
-           " ".join(str(t) for t in myo_tags))
-          
-    os.system(cmd)
-    
-    # smooth the surface surrounding the myocardium
-    cmd = "%s smooth surface -msh=%s -surf=%s-outer -outmsh=%s -ofmt=carp_txt -thr=0.1 -smth=0.15 -iter=50"%
-          (meshtool_bin, mesh_name, mesh_name, smooth_mesh)
-    
-    os.system(cmd)
-  
+        # extract surface surrounding the myocardium
+        cmd = "{} extract surface -msh={} -surf={}-outer -op={} -ofmt=carp_txt".format(meshtool_bin,
+                                                                                       mesh_name,
+                                                                                       mesh_name,
+                                                                                       " ".join(
+                                                                                           str(t) for t in myo_tags))
+        os.system(cmd)
+
+        # smooth the surface surrounding the myocardium
+        cmd = "{} smooth surface -msh={} -surf={}-outer -outmsh={} -ofmt=carp_txt -thr=0.1 -smth=0.15 -iter=50".format(
+            meshtool_bin,
+            mesh_name,
+            mesh_name,
+            smooth_mesh)
+        os.system(cmd)
--- post_process_seg.py
+++ post_process_seg.py
@@ -2,97 +2,94 @@ import os.path
 import numpy as np
 
 
-def smooth_seg(segsmooth_bin, original_seg, smooth_seg, tags_list, target_res, fuzz_factor = 1.0, voxel_padding = 10):
-  
-  ''' Smooth segmentation using segsmooth. Segsmooth is part of the segtools package (MUG). Expects NRRD files.
-      
-      Parameters: 
-        segsmooth_bin (string): path+name of segsmooth binary
-        original_seg (string): path+name of original segmentation's NRRD file
-        smooth_seg (string): path+name of output smooth segmentation's NRRD file
-        tags_list (list): list of tags to be smoothed. Example: [2, 4, 6]
-        target_res (float): target resolution in milimeters. Ex. 0.25
-        fuzz_factor (float): factor defining how much smoothing is applied. Default = 1.0 (Optional)
-        voxel_padding (int): number of voxels to pad the smooth segmentation with. Default = 10 (Optional)
-      Returns:
-        Writes out a new smooth segmentation to NRRD file
-  ''' 
-  
-  if os.path.isfile(original_seg) and not os.path.isfile(smooth_seg):
-    print("Smoothing segmentation...")
+def smooth_segmentation(segsmooth_bin, original_seg, smooth_seg, tags_list, target_res, fuzz_factor=1.0, voxel_padding=10):
 
-    cmd = "%s %s %s -l %s --fuzz-factor %1.1f --voxel-units --padding %d %d %d -r %1.2f %1.2f %1.2f"% 
-          (segsmooth_bin, 
-           original_seg, 
-           smooth_seg, 
-           " ".join(str(t) for t in tags_list), 
-           fuzz_factor,
-           "".join((str(voxel_padding)+' ')*3),
-           "".join((str(target_res)+' ')*3))
-           
-    print(cmd)
-    os.system(cmd)      
+    """ Smooth segmentation using segsmooth. Segsmooth is part of the segtools package (MUG). Expects NRRD files.
+
+        Parameters:
+          segsmooth_bin (string): path+name of segsmooth binary
+          original_seg (string): path+name of original segmentation's NRRD file
+          smooth_seg (string): path+name of output smooth segmentation's NRRD file
+          tags_list (list): list of tags to be smoothed. Example: [2, 4, 6]
+          target_res (float): target resolution in millimeters. Ex. 0.25
+          fuzz_factor (float): factor defining how much smoothing is applied. Default = 1.0 (Optional)
+          voxel_padding (int): number of voxels to pad the smooth segmentation with. Default = 10 (Optional)
+        Returns:
+          Writes out a new smooth segmentation to NRRD file
+    """
+  
+    if os.path.isfile(original_seg) and not os.path.isfile(smooth_seg):
+        print("Smoothing segmentation...")
+
+        cmd = "{} {} {} -l {} --fuzz-factor {} --voxel-units --padding {} -r {}".format(
+            segsmooth_bin,
+            original_seg,
+            smooth_seg,
+            " ".join(str(t) for t in tags_list),
+            fuzz_factor,
+            "".join((str(voxel_padding)+' ')*3),
+            "".join((str(target_res)+' ')*3))
+
+        print(cmd)
+        os.system(cmd)
 
 
 def tag_base_with_plane(segclip_bin, original_seg, base_seg, plane_name, tags_list, base_tag):
-  
-  ''' Tag base of segmentation using a plane. 
-      Uses segclip, which is part of the segtools package (MUG). 
-      Expects NRRD files and a text file with plane definition.
-      
-      Parameters: 
-        segclip_bin (string): path+name of segclip binary
-        original_seg (string): path+name of original segmentation's NRRD file
-        base_seg (string): path+name of output tagged segmentation's NRRD file
-        plane_name (string): path+name of text file containing a plane's origin and normal. 
-                             Format: line 1 contains the origin point and line 2 contains the normal vector ("x y z")
-        tags_list (list): list of tags to be labeled as base. Example: [2, 4, 6]
-        base_tag (int): tag to label the base
-      Returns:
-        Writes out a new segmentation file (NRRD) with the tagged base
-  ''' 
-  
-  # read plane
-  plane = np.loadtxt(plane_name,dtype=float)
-  p0 = plane[0,:]
-  v  = plane[1,:]
 
-  # tag image
-  if os.path.isfile(original_seg) and not os.path.isfile(base_seg):	
-    for tag in tags_list:
-      print("Tagging base of tag %d..."%tag)
+    """ Tag base of segmentation using a plane.
+        Uses segclip, which is part of the segtools package (MUG).
+        Expects NRRD files and a text file with plane definition.
 
-      cmd = "%s %s %s --clip-tag %d --to-tag %d --origin %s --normal %s"%
-            (segclip_bin,
-            original_seg,
-            base_seg,
-            tag,
-            base_tag,
-            " ".join(str(e) for e in p0)
-            " ".join(str(e) for e in v))
-      
-      print(cmd)
-      os.system(cmd)
+        Parameters:
+          segclip_bin (string): path+name of segclip binary
+          original_seg (string): path+name of original segmentation's NRRD file
+          base_seg (string): path+name of output tagged segmentation's NRRD file
+          plane_name (string): path+name of text file containing a plane's origin and normal.
+                               Format: line 1 contains the origin point and line 2 contains the normal vector ("x y z")
+          tags_list (list): list of tags to be labeled as base. Example: [2, 4, 6]
+          base_tag (int): tag to label the base
+        Returns:
+          Writes out a new segmentation file (NRRD) with the tagged base
+    """
+  
+    # read plane
+    plane = np.loadtxt(plane_name, dtype=float)
+    p0 = plane[0, :]
+    v = plane[1, :]
+
+    # tag image
+    if os.path.isfile(original_seg) and not os.path.isfile(base_seg):
+        for tag in tags_list:
+            print("Tagging base of tag {:d}...".format(tag))
+
+            cmd = "{} {} {} --clip-tag {} --to-tag {} --origin {} --normal {}".format(segclip_bin,
+                                                                                      original_seg,
+                                                                                      base_seg,
+                                                                                      tag,
+                                                                                      base_tag,
+                                                                                      " ".join(str(e) for e in p0),
+                                                                                      " ".join(str(e) for e in v))
+            print(cmd)
+            os.system(cmd)
         
           
 def add_bath_to_seg(segchangetag_bin, original_seg, bath_seg, bath_tag):
 
-  ''' Add bath to segmentation by changing the "0" tag to a user-defined number. 
-      Uses segchangetag, which is part of the segtools package (MUG). 
-      Expects NRRD files.
-      
-      Parameters: 
-        segchangetag_bin (string): path+name of segchangetag binary
-        original_seg (string): path+name of original segmentation's NRRD file
-        bath_seg (string): path+name of output tagged segmentation's NRRD file
-        bath_tag (int): tag to label the bath
-      Returns:
-        Writes out a new segmentation file (NRRD) with a bath with tag bath_tag
-  ''' 
-  
-  # add bath by changing tag
-  if os.path.isfile(original_seg) and not os.path.isfile(bath_seg):
-    print("Adding bath to original image...")
-    cmd = "%s %s %s 0 %d" % (segchangetag_bin, original_seg, bath_seg, bath_tag)
-    os.system(cmd)          
+    """ Add bath to segmentation by changing the "0" tag to a user-defined number.
+        Uses segchangetag, which is part of the segtools package (MUG).
+        Expects NRRD files.
 
+        Parameters:
+          segchangetag_bin (string): path+name of segchangetag binary
+          original_seg (string): path+name of original segmentation's NRRD file
+          bath_seg (string): path+name of output tagged segmentation's NRRD file
+          bath_tag (int): tag to label the bath
+        Returns:
+          Writes out a new segmentation file (NRRD) with a bath with tag bath_tag
+    """
+  
+    # add bath by changing tag
+    if os.path.isfile(original_seg) and not os.path.isfile(bath_seg):
+        print("Adding bath to original image...")
+        cmd = "{} {} {} 0 {:d}".format(segchangetag_bin, original_seg, bath_seg, bath_tag)
+        os.system(cmd)
